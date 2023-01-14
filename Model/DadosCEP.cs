99ed8921dd5b18b7b089669d233dd6619c3adc9d
Eli Neto <43022198+EliNeto21@users.noreply.github.com>
2023-01-13 23:21:32 -0300

    Add files via upload

---
+++ Model/DadosCEP.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class DadosCEP
+    {
+        public string Code { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public string District { get; set; }
+        public string Address { get; set; }
+        public int Status { get; set; }
+    }
+}
