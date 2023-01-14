090c0ce97e9635a98d510154aaa7a9ad4df65be3
Eli Neto <elineto91@gmail.com>
2023-01-13 23:19:40 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Model;
+using System;
+using System.Diagnostics;
+using TratamentoDados;
+
+namespace WebApp_CEP.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            string nrCEP = Request.Query["nrCEP"];
+            var cep = new DadosCEP();
+
+            if (nrCEP != null)
+            {
+                cep = ConsultaCEP(nrCEP);
+            }
+
+            ViewBag.dadosCEP = cep;
+            return View();
+        }
+
+        public static DadosCEP ConsultaCEP(string nrCEP)
+        {
+            var cep = new DadosCEP();
+            cep = DadosCEPController.BuscaDados(nrCEP);
+
+            return cep;
+        }
+    }
+}
